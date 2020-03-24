76200c76a9e117f8142a0ecd0c430a537df6f36c
Fabiana Zioti <fabi_zioti@hotmail.com>
2020-03-24 10:46:12 -0300

    adding drives factory

---
+++ sample_db_utils/drives/factory_driver.py
@@ -0,0 +1,26 @@
+#
+# This file is part of Sample Database Utils.
+# Copyright (C) 2019 INPE.
+#
+# Sample Database Utils is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+""" Sample DB Utils Drives Factory"""
+
+from .inSitu import InSitu
+
+
+class DriversFactory:
+    """Factory for Drives."""
+
+    @staticmethod
+    def make(driverType, entries, storager):
+        """Factory method for creates datasource."""
+        factorys = ["Cerrado", "VMaus", "Canasat", "Lapig", "Fototeca" , "Embrapa", "InSitu"]
+
+        assert driverType in factorys
+
+        driver = eval(driverType)(entries, storager)
+
+        return driver

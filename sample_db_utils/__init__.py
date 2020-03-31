6a3806c26cb398ebb514a86f11a2ed8cda99afe2
Fabiana Zioti <fabi_zioti@hotmail.com>
2020-03-31 11:48:21 -0300

    adding BDC init

--- sample_db_utils/__init__.py
+++ sample_db_utils/__init__.py
@@ -11,6 +11,7 @@
 from .version import __version__
 from .core.postgis_accessor import PostgisAccessor
 from .drives.inSitu import InSitu
+from .drives.bdc import BDC
 from .drives.factory_driver import DriversFactory
 
-__all__ = ('__version__', 'InSitu', 'PostgisAccessor', 'DriversFactory', )
+__all__ = ('__version__', 'InSitu', 'PostgisAccessor', 'DriversFactory', 'BDC')
